aa12893dd5028e5c4d455a3f594c1e7e1804567d
FALZ <falz@nifty.com>
2015-04-08 00:14:22 +0900

    * Ruby のインストール後に以下の処理を追加した。   * gem のアップデート   * Bundler のインストール  * Ruby のインストールは同じバージョンがインストールされていたら行わないようにした。

--- site-cookbooks/ruby-src-install/attributes/default.rb
+++ site-cookbooks/ruby-src-install/attributes/default.rb
@@ -12,3 +12,5 @@ default['ruby-src-install']['version'] = '2.1.5'
 default['ruby-src-install']['major_version'] = default['ruby-src-install']['version'].split('.')[0..1].join('.')
 default['ruby-src-install']['url'] = "http://cache.ruby-lang.org/pub/ruby/#{default['ruby-src-install']['major_version']}"
 default['ruby-src-install']['checksum'] = '5a4de38068eca8919cb087d338c0c2e3d72c9382c804fb27a'
+
+default['ruby-src-install']['gem_packages'] = %w{bundler}
--- site-cookbooks/ruby-src-install/recipes/default.rb
+++ site-cookbooks/ruby-src-install/recipes/default.rb
@@ -8,30 +8,42 @@
 #
 
 package 'ruby' do
-  action :remove
+	action :remove
 end
 
 include_recipe 'build-essential'
 node['ruby-src-install']['required_packages'].each do |pkg|
-   package pkg
+	package pkg
 end
 
 version = node['ruby-src-install']['version']
 cache_path = Chef::Config[:file_cache_path]
 
 remote_file "#{cache_path}/ruby-#{version}.tar.gz" do
-   source "#{node['ruby-src-install']['url']}/ruby-#{version}.tar.gz"
-   checksum node['ruby-src-install']['checksum']
+	source "#{node['ruby-src-install']['url']}/ruby-#{version}.tar.gz"
+	checksum node['ruby-src-install']['checksum']
+	not_if { File.exists?("#{cache_path}/ruby-#{version}.tar.gz") }
 end
 
 configure_options = node['ruby-src-install']['configure_options'].join(' ')
 
 execute "build-ruby" do
-   command <<-EOF
-      cd  #{cache_path}
-      tar xfz ruby-#{version}.tar.gz 
-      cd ruby-#{version}
-      ./configure #{configure_options} 
-      make install
-   EOF
+	cwd #{cache_path}
+	command <<-EOF
+		tar xfz ruby-#{version}.tar.gz 
+		cd ruby-#{version}
+		./configure #{configure_options} 
+		make install
+	EOF
+	# 同じバージョンがインストールされていたら何もしない
+	not_if "ruby -v | grep #{version}"
+end
+
+execute "update gem" do
+	command "gem update --system"
+	action :run
+end
+
+node['ruby-src-install']['gem_packages'].each do |gem_pkg|
+	gem_package gem_pkg
 end
