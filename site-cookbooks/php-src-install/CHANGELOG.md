b44aff38812638b9032968a699cf71684e80a679
FALZ <falz@nifty.com>
2016-09-30 02:43:47 +0000

    インストールするPHPのバージョンを 5.6.26 にした。

--- site-cookbooks/php-src-install/CHANGELOG.md
+++ site-cookbooks/php-src-install/CHANGELOG.md
@@ -1,6 +1,9 @@
 php-src-install CHANGELOG
 =========================
 
+0.1.1
+-----
+- [falz-yineng] - PHP version up (5.6.4 -> 5.6.26)
 0.1.0
 -----
 - [falz-yineng] - Initial release of php-src-install
--- site-cookbooks/php-src-install/attributes/default.rb
+++ site-cookbooks/php-src-install/attributes/default.rb
@@ -1,4 +1,4 @@
-default['php-src-install']['version'] = '5.6.4'
+default['php-src-install']['version'] = '5.6.26'
 default['php-src-install']['tarball'] = "php-#{default['php-src-install']['version']}.tar.gz"
 default['php-src-install']['url'] = "http://jp2.php.net/distributions/#{default['php-src-install']['tarball']}"
 default['php-src-install']['complile_options'] = %w{
